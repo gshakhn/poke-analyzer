ecd938c989e84dbb0d9c9757e573614787f1741f
George Shakhnazaryan <gshakhn@gmail.com>
2016-07-24 16:46:25 -0500

    Make Main method print as it goes and handle errors semi-gracefully.

--- src/main/scala/com/gshakhn/pokeanalyzer/Main.scala
+++ src/main/scala/com/gshakhn/pokeanalyzer/Main.scala
@@ -1,17 +1,18 @@
 package com.gshakhn.pokeanalyzer
 
-import ammonite.ops._
 import ammonite.ops.ImplicitWd._
+import ammonite.ops._
+
+import scala.util.control.NonFatal
 
 object Main extends App {
   val inputDirectory = cwd / "input"
-  val pokemon = ls ! inputDirectory map  { inputFile =>
-    new PokeParser(inputFile).parse
+  ls ! inputDirectory foreach { inputFile =>
+    try {
+      val pokemon = new PokeParser(inputFile).parse
+      write.append(cwd/"pokemon-info.txt", s"${pokemon.name},${pokemon.cp},${pokemon.hp},${pokemon.dust}\n")
+    } catch {
+      case NonFatal(e) => println(s"Could not parse ${inputFile.last}")
+    }
   }
-
-  pokemon.foreach(poke =>
-    write.append(cwd/"pokemon-info.txt", s"${poke.name},${poke.cp},${poke.hp},${poke.dust}\n")
-  )
 }
-
-case class InputToOutput(input: Path, output: Path)
