412c33769bca82ca253734c7915f2f1a15305cd3
George Shakhnazaryan <gshakhn@gmail.com>
2016-07-24 21:30:51 -0500

    Take trainer level as a command line argument.

--- src/main/scala/com/gshakhn/pokeanalyzer/Main.scala
+++ src/main/scala/com/gshakhn/pokeanalyzer/Main.scala
@@ -5,11 +5,12 @@ import ammonite.ops._
 import scala.util.control.NonFatal
 
 object Main extends App {
-  val inputDirectory = Path(args(0))
+  val trainerLevel = args(0).toInt
+  val inputDirectory = Path(args(1))
   ls ! inputDirectory foreach { inputFile =>
     try {
-      val pokemon = new ScreenshotParser(inputFile, 23).parse
-      val perfectionRange = new PokeIVCalculator(23).perfectionRange(pokemon)
+      val pokemon = new ScreenshotParser(inputFile, trainerLevel).parse
+      val perfectionRange = new PokeIVCalculator(trainerLevel).perfectionRange(pokemon)
       val min = perfectionRange._1.formatted("%.2f")
       val max = perfectionRange._2.formatted("%.2f")
       println(s"${pokemon.name},${pokemon.cp},${pokemon.hp},${pokemon.dust},${pokemon.level}, $min - $max")
